ad5c3636e4aa2687d420fbb4676467ee7f67b846
MM10001000 <158249668+MM10001000@users.noreply.github.com>
2024-07-31 06:09:15 +0300

    Create FixMe.java

---
+++ FixMe.java
@@ -0,0 +1,37 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixMe {
+    public static void main(String[] args) {
+        // Test the method with different inputs
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(-1);
+        list.add(3);
+        list.add(-4);
+        list.add(-5);
+        list.add(6);
+
+        removeNegatives(list);
+        System.out.println(list);}
+    
+
+    /**
+     * The method removes all elements smaller than zero from the list
+     * @param list the list from which elements are removed
+     */
+    public static void removeNegatives(ArrayList<Integer> list) {
+        int index = 0;
+        while( index < list.size()) {
+            AbstractList<E> list;
+            Object index;
+            int element = list.get(index);
+            if (element > 0) {
+                list.remove(index);
+            } else {
+                index++;
+            }
+        }
+    }
